31163c6028b12a308114c58a0b0d5115deca5b1f
Nathan Duriez <NathanDuriez@protonmail.com>
2022-03-01 23:01:27 +0100

    Classe ShoppingList

---
+++ src/ShoppingList.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+public class ShoppingList extends Vector<Product> {
+
+    private int id;
+    private String name;
+
+    private boolean isArchived = false;
+
+    ShoppingList(String name) {this.name = name;}
+
+    ShoppingList(String name, int id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {return id;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public boolean isArchived() {return isArchived;}
+    public void setArchived(boolean isArchived) {this.isArchived = isArchived;}
+
+}
