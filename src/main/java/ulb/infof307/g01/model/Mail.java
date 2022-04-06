0bf50015dcc6185285ba1d31f5fc2ac3a91a7cbb
Nathan Duriez <NathanDuriez@protonmail.com>
2022-04-06 17:30:44 +0200

    JavaDoc
    Co-authored-by: Smekarni <salma.mekarnia@ulb.be>

--- src/main/java/ulb/infof307/g01/model/Mail.java
+++ src/main/java/ulb/infof307/g01/model/Mail.java
@@ -1,10 +1,15 @@
 package ulb.infof307.g01.model;
 
+import ulb.infof307.g01.db.PDFCreator;
+
 import javax.activation.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 import java.util.Properties;
 
+/**
+ * Classe permettant d'envoyer la liste de course en PDF par Mail, utilise la librairie {@link javax.mail}.
+ */
 public class Mail {
     private final static String defaultAddress = "autochef@outlook.fr";
     private final static String defaultAddressPassword = "43fZ5LD4gEJk74B";
@@ -15,16 +20,27 @@ public class Mail {
 
     private Session session;
 
+
     public Mail() {
         createSession();
     }
 
+    /**
+     * Envoie une @{code ShoppingList} par mail.
+     * @param recipientAddress Adresse e-mail du destinataire du mail.
+     * @param shoppingListToSend Liste de Course à envoyé (en pièce-jointe).
+     * @throws AddressException Si l'adresse e-mail n'est pas dans un format valide.
+     * @throws MessagingException Pour toutes les autres erreurs.
+     */
     public void sendMail(String recipientAddress, ShoppingList shoppingListToSend) throws MessagingException {
         createSession();
         Message mail = composeMessage(recipientAddress, shoppingListToSend);
         Transport.send(mail);
     }
 
+    /**
+     * Initialise la Session (Host SMTP, Port du serveur SMTP, Authentification, Chiffrement,...).
+     */
     private void createSession() {
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
@@ -39,6 +55,14 @@ public class Mail {
         });
     }
 
+    /**
+     * Créer un {@link Message} contenant la {@code ShoppingList} en pièce-jointe.
+     * @param recipientAddress Adresse e-mail du destinataire du mail.
+     * @param shoppingListToSend Liste de Course à envoyé (en pièce-jointe).
+     * @return le {@code Message} créé.
+     * @throws AddressException Si l'adresse e-mail n'est pas dans un format valide.
+     * @throws MessagingException Pour toutes les autres erreurs.
+     */
     private Message composeMessage(String recipientAddress, ShoppingList shoppingListToSend) throws AddressException, MessagingException {
         Message message = new MimeMessage(session);
 
@@ -57,6 +81,11 @@ public class Mail {
         return message;
     }
 
+    /**
+     * Créer une {@link BodyPart} contenant le texte du mail.
+     * @return la {@code BodyPart} créée.
+     * @throws MessagingException si une exception est déclenchée par la librairie.
+     */
     private BodyPart addText() throws MessagingException {
         BodyPart textBodyPart = new MimeBodyPart();
         textBodyPart.setText("On vous as envoyé cette Liste de Course en pièce jointe");  // TODO : nom Liste de courses
