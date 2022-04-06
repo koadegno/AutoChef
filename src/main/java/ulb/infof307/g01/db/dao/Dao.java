96171bfc1901512d0f97a86acfcdc5d398183c8b
Helin <demirel.helin01@gmail.com>
2022-04-06 22:21:56 +0200

    creation package dao

--- src/main/java/ulb/infof307/g01/db/dao/Dao.java
+++ src/main/java/ulb/infof307/g01/db/dao/Dao.java
@@ -4,7 +4,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Interface d'accès aux méthodes de la base de données
+ * Contient les méthodes générales de lecture et d'écriture
+ * @param <T>
+ */
 public interface Dao<T> {
     List<String> getAllName() throws SQLException;
     void insert(T t) throws SQLException;
