7c26e3b313b15ed76765d10293b5f3cf293d72eb
imoussao27 <imane.moussaoui@ulb.be>
2022-04-08 15:14:31 +0200

    [ADD] verif si le mail existe
    Co-authored-by: nduriez <NathanDuriez@protonmail.com>
    Co-authored-by: hdemirel01 <demirel.helin01@gmail.com>

--- src/main/java/ulb/infof307/g01/ui/mail/FavoriteMailView.java
+++ src/main/java/ulb/infof307/g01/ui/mail/FavoriteMailView.java
@@ -6,19 +6,51 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import ulb.infof307.g01.ui.Window;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 public class FavoriteMailView extends Window {
     public VBox vBox;
     public TextField newFavoriteMail;
     private ComboBox<String> comboboxListFavoriteMaiL;
 
 
-    public void confirmFavoriteMail() {
-        //TODO:ajouter le mail a la base de donnee
+    public void confirmMail() {
         //TODO:verifier si c'est bien écrit
+        setNodeColor(newFavoriteMail, false);
         String newMail = newFavoriteMail.getText(); //envoyer ça
-        comboboxListFavoriteMaiL.setPromptText(newMail); //TODO: changer solution poubelle
-        Stage stage = (Stage) vBox.getScene().getWindow();
-        stage.close();
+        if(isValidEmailAddress(newMail)){
+            comboboxListFavoriteMaiL.setPromptText(newMail); //TODO: changer solution poubelle
+            Stage stage = (Stage) vBox.getScene().getWindow();
+            stage.close();
+        }
+        else{
+            setNodeColor(newFavoriteMail, true);
+        }
+
+    }
+
+    public void addFavoriteMail(){
+        String newMail = newFavoriteMail.getText();
+        setNodeColor(newFavoriteMail, false);
+        if(isValidEmailAddress(newMail)){
+            //TODO:ajouter le mail a la base de donnee
+            confirmMail();
+        }
+        else{
+            setNodeColor(newFavoriteMail, true);
+        }
+    }
+
+    public boolean isValidEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
     }
 
     public void setComboboxListFavoriteMail(ComboBox<String> comboboxListFavoriteMaiL){
