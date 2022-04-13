e21670b55e72a0327a8812ec3cbeb42270d281a1
Nathan Duriez <NathanDuriez@protonmail.com>
2022-04-13 23:31:02 +0200

    Javadoc Main + suppresion d'un warning
    Co-authored-by: hdemirel01 <demirel.helin01@gmail.com>

--- src/main/java/ulb/infof307/g01/Autochef.java
+++ src/main/java/ulb/infof307/g01/Autochef.java
@@ -1,13 +1,16 @@
-package ulb.infof307.g01.view;
+package ulb.infof307.g01;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import ulb.infof307.g01.controller.MainController;
 
+/**
+ * Vrai classe {@code Main}, point d'entrée du programme
+ */
 public class Autochef extends Application {
 
     public  void launchApp(String[] args){
-        this.launch(args);
+        launch(args);
     }
     @Override
     public void start(Stage primaryStage){
