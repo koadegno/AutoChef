a42381ca424f927a612c092e9ef32426e3460abc
Nathan Duriez <NathanDuriez@protonmail.com>
2022-04-18 23:41:03 +0200

    Install Directory jnlibs

--- src/main/java/ulb/infof307/g01/Main.java
+++ src/main/java/ulb/infof307/g01/Main.java
@@ -1,13 +1,20 @@
 package ulb.infof307.g01;
 
 
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+
 /**
  * Classe qui sert seulement à lancer l'application
  * <br>
  * Nécessaire pour faire un JAR avec {@code JavaFX}
  */
 public class Main  {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        String path = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        path = path.replace("g01-iteration-2.jar", "jniLibs/");
+
+        ArcGISRuntimeEnvironment.setInstallDirectory(path);
         Autochef app = new Autochef();
         app.launchApp(args);
     }
