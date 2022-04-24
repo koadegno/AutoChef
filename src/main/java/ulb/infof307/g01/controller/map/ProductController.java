d1423daab7fcf18e9d362d100dacf19230166046
imoussao27 <imane.moussaoui@ulb.be>
2022-04-24 17:14:07 +0200

    [ADD] init information combobox

--- src/main/java/ulb/infof307/g01/controller/map/ProductController.java
+++ src/main/java/ulb/infof307/g01/controller/map/ProductController.java
@@ -2,23 +2,60 @@ package ulb.infof307.g01.controller.map;
 
 import javafx.stage.Stage;
 import ulb.infof307.g01.controller.Controller;
+import ulb.infof307.g01.model.database.Configuration;
 import ulb.infof307.g01.view.ViewController;
 import ulb.infof307.g01.view.shop.ProductViewController;
 import ulb.infof307.g01.view.shop.ShopViewController;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ProductController extends Controller implements ProductViewController.Listenner {
-    private ProductViewController productViewController;
+    private final ProductViewController productViewController;
+
+    public ProductController(){
+        productViewController = new ProductViewController();
+    }
 
     public void displayCreateNewProduct(){
         String nameCreateProductFXML = "createProduct.fxml";
         try {
-            Stage shopStage = popupFXML(nameCreateProductFXML, productViewController);
+            Stage createProductStage = popupFXML(nameCreateProductFXML, productViewController);
             productViewController.setListener(this);
-            setStage(shopStage);
+            this.initCreateProductFXML();
+            //setStage(createProductStage);
         } catch (IOException e) {
             ViewController.showErrorFXMLMissing(nameCreateProductFXML);
         }
+    }
+
+    public void initCreateProductFXML(){
+        ArrayList<String> nameProductFamily = null;
+        ArrayList<String> nameProductUnity = null;
+        try {
+            nameProductFamily = Configuration.getCurrent().getProductFamilyDao().getAllName();
+            nameProductUnity = Configuration.getCurrent().getProductUnityDao().getAllName();
+
+        } catch (SQLException e) {
+            //TODO: régler cette erreur
+            e.printStackTrace();
+        }
+        productViewController.initComboboxInformation(nameProductFamily,nameProductUnity);
+    }
+
+    @Override
+    public void confirmCreateProduct() {
+        //TODO: appeler la base de donnée pour lui envoyer le produit
+    }
+
+    @Override
+    public void ImportProductJsonFile() {
+
+    }
+
+    @Override
+    public void ReturnShopPage() {
+
     }
 }
--- src/main/java/ulb/infof307/g01/view/shop/ProductViewController.java
+++ src/main/java/ulb/infof307/g01/view/shop/ProductViewController.java
@@ -1,13 +1,37 @@
 package ulb.infof307.g01.view.shop;
 
+import javafx.collections.FXCollections;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import ulb.infof307.g01.view.ViewController;
+
+import java.util.ArrayList;
 
 public class ProductViewController extends ViewController {
 
+    public ComboBox<String> nameProductFamilyCombobox, nameProductUnityCombobox;
+    public TextField nameProduct;
 
+    public void initComboboxInformation(ArrayList<String> nameProductFamily, ArrayList<String> nameProductUnity){
+        nameProductFamilyCombobox.setItems(FXCollections.observableArrayList(nameProductFamily));
+        nameProductUnityCombobox.setItems(FXCollections.observableArrayList(nameProductUnity));
+    }
 
+    public void confirmCreateProduct(){
+
+    }
+
+    public void ImportProductJSONFile(){
+
+    }
+
+    public void ReturnShopPage(){
+
+    }
 
     public interface Listenner{
-
+        void confirmCreateProduct();
+        void ImportProductJsonFile();
+        void ReturnShopPage();
     }
 }
