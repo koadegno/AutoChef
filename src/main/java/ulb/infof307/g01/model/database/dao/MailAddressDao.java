b03d5ad5903bc45055fc724298a4c71abb0589a9
koadegno <vinove9@hotmail.fr>
2022-04-25 17:45:22 +0200

    utilisateur mail favori fait et fonctionnelle
    Co-authored-by: aiabdoul <aissa.abdoul-aziz@gmail.com>

--- src/main/java/ulb/infof307/g01/model/database/dao/MailAddressDao.java
+++ src/main/java/ulb/infof307/g01/model/database/dao/MailAddressDao.java
@@ -13,6 +13,8 @@ import java.util.List;
  */
 public class MailAddressDao extends Database implements Dao<String> {
     public static final String TABLE_MAIL_ADDRESS = "AdresseMail";
+    public static final String TABLE_USER_MAIL_ADDRESS = "UtilisateurMailFavoris";
+
 
     //TODO MODIFIER STRING Pour mettre une classe Adresse mail ! si besoin
     /**
@@ -30,13 +32,65 @@ public class MailAddressDao extends Database implements Dao<String> {
         return getAllNameFromTable(TABLE_MAIL_ADDRESS,"ORDER BY Nom ASC");
     }
 
+    /**
+     * insert une adresse mail dans la base de donnée
+     * @param mailAddressName l'adresse mail a inséré
+     * @throws SQLException erreur liée à de la requête
+     */
     @Override
     public void insert(String mailAddressName) throws SQLException {
-        insert(TABLE_MAIL_ADDRESS, new String[]{"null",String.format("'%s'",mailAddressName)});
+        List<String> mailsInserted = getAllName();
+        if(!mailsInserted.contains(mailAddressName)){
+            insert(TABLE_MAIL_ADDRESS, new String[]{"null",String.format("'%s'",mailAddressName)});
+        }
+        else{
+            //TODO renvoyer une erreur
+            System.out.println("Le mail existe deja bg");
+        }
 
     }
 
+    /**
+     * Insert une adresse mail pour un utilisateur spécifique
+     * @param mailAddressName l'addresse mail a inséré
+     * @param userID l'id de l'utilisateur
+     * @throws SQLException exception lié à la base de donnée
+     */
+    public void insert(String mailAddressName,int userID) throws SQLException {
+        List<String> mailsInserted = getAllName(userID);
+        if(!mailsInserted.contains(mailAddressName)){
+            insert(TABLE_MAIL_ADDRESS, new String[]{"null",String.format("'%s'",mailAddressName)});
+            int mailID = getGeneratedID();
+            insert(TABLE_USER_MAIL_ADDRESS, new String[]{String.valueOf(userID),String.valueOf(mailID)});
+        }
+        else{
+            //TODO renvoyer une erreur
+            System.out.println("Le mail existe deja bg");
+        }
+    }
+
+    /**
+     * Renvoyer toutes les adresses mails favoris d'un utilisateur en particulier
+     * @param userID l'identifiant de l'utilisateur
+     * @return la liste de tous les mails. Peut-être vide
+     * @throws SQLException erreur lié à la base de donnée
+     */
+    public List<String> getAllName(int userID) throws SQLException {
+        String query = String.format("""
+                SELECT AdresseMail.Nom
+                FROM UtilisateurMailFavoris as UMF
+                INNER JOIN AdresseMail ON UMF.AdresseMailID = AdresseMail.AdresseMailID
+                WHERE UMF.UtilisateurID = %d""",userID);
+        ResultSet querySelectUserMail = sendQuery(query);
+        List<String> userMails = new ArrayList<>();
+        while (querySelectUserMail.next()){ // attention peut etre vide
+            userMails.add(querySelectUserMail.getString(1));
+        }
+        return userMails;
+    }
+
     @Override
+    @Deprecated
     public void update(String s) throws SQLException{
         throw new IllegalCallerException("Cette methode n'est pas implementé");
     }
