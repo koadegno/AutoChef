363ac5a992f8c0f086ecf972abd2349da551acad
imoussao27 <imane.moussaoui@ulb.be>
2022-04-30 18:22:49 +0200

    [REF] refactor MVC shoppingListController done
    Co-authored-by: koadegno <koadegno@users.noreply.github.com>

--- src/main/java/ulb/infof307/g01/controller/shoppingList/ShoppingListController.java
+++ src/main/java/ulb/infof307/g01/controller/shoppingList/ShoppingListController.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
 public class ShoppingListController extends Controller implements ShoppingListViewController.Listener {
     protected UserShoppingListViewController userShoppingListViewController;
     protected CreateUserShoppingListViewController createUserShoppingListViewController;
