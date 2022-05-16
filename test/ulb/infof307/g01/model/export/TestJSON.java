39a39051e4a0a83492ffec45e8170ef12f69151c
aiabdoul <aissa.abdoul-aziz@ulb.be>
2022-05-16 22:19:22 +0200

    [fix]  fix test json starting 0
    Co-authored-by: koadegno<koadegno@users.noreply.github.com>

--- test/ulb/infof307/g01/model/export/TestJSON.java
+++ test/ulb/infof307/g01/model/export/TestJSON.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
@@ -53,17 +55,52 @@ class TestJSON {
         fileJSON.flush();
     }
 
-//    @BeforeAll @SuppressWarnings("unchecked") //Warning pour le put car librairie pas a jour.
-//    static public void createJSONProduct() throws IOException {
-//        JSONObject product = new JSONObject();
-//        product.put("Nom", "testProduit");
-//        product.put("FamilleAliment", "soupes");
-//        product.put("Unite", "l");
-//
-//        FileWriter fileJSON = new FileWriter(fileNameProduct);
-//        fileJSON.write(product.toJSONString());
-//        fileJSON.flush();
-//    }
+@BeforeAll @SuppressWarnings("unchecked") //Warning pour le put car librairie pas a jour.
+ static public void createJSONProduct() throws IOException {
+        JSONObject job = new JSONObject();
+
+    // LinkedHashMap is created for address data
+        Map m1 = new LinkedHashMap(4);
+        m1.put("Unite", "l");
+        m1.put("FamilleAliment", "soupes");
+        m1.put("Nom", "miam2");
+
+    // adding address to the created JSON object
+        job.put("address1", m1);
+    // JSONArray is created to add the phone numbers
+        JSONArray jab = new JSONArray();
+        m1 = new LinkedHashMap(2);
+        m1.put("type1", "home1");
+        m1.put("no", "9738128018");
+    // adding map to list
+        jab.add(m1);
+        m1 = new LinkedHashMap(2);
+        m1.put("type2", "fax1");
+        m1.put("no1", "6366182095");
+    // map is added to the list
+        jab.add(m1);
+    // adding phone numbers to the created JSON object
+        job.put("phoneNos", jab);
+    // the JSON data is written into the file
+        PrintWriter pwt = new PrintWriter("JSON.json");
+        pwt.write(job.toJSONString());
+        pwt.flush();
+        pwt.close();
+
+
+
+        FileWriter fileJSON = new FileWriter(fileNameProduct);
+        fileJSON.write(product.toJSONString());
+       fileJSON.flush();
+    {"Unite":"l","FamilleAliment":"soupes","Nom":"testProduit"}
+
+    {"Liste" : [
+        {"Unite":"l","FamilleAliment":"soupes","Nom":"miam2"},
+        {"Unite":"l","FamilleAliment":"soupes","Nom":"miam"}
+        ]
+    }
+
+}
 
     @AfterAll
     static public void deleteDB() throws IOException, SQLException {
