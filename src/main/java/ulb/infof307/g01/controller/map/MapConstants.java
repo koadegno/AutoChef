313ec4339272163aa43d82a706a1c8fcb6cc51e3
koadegno <vinove9@hotmail.fr>
2022-05-16 19:37:01 +0200

    [REFACTOR] refactoring

--- src/main/java/ulb/infof307/g01/controller/map/MapConstants.java
+++ src/main/java/ulb/infof307/g01/controller/map/MapConstants.java
@@ -1,18 +1,26 @@
 package ulb.infof307.g01.controller.map;
 
 /**
- * Définie toutes les constantes utilisées a travers le package map
+ * Définie toutes les constantes utilisées à travers le package map
  */
 public final class MapConstants {
     public static final int COLOR_RED = 0xFFFF0000;
     public static final int COLOR_BLACK = 0xFF000000;
     public static final int COLOR_BLUE   = 0xFF008DFF;
+    public static final String API_KEY = "AAPK7d69dbea614548bdb8b6096b100ce4ddBX61AYZWAVLJ-RF_EEw68FrqS-y9ngET8KMzms5ZERiMTtShQeDALmWawO0LcM1S";
+    public static final int SIZE = 10;
+    public static final int MAX_RESULT = 1;
+    public static final double LATITUDE_BRUSSELS = 50.85045;
+    public static final double LONGITUDE_BRUSSELS = 5.34878;
+    public static final double MAP_SCALE = 4000000.638572;
+
     static final int ADDRESS_SIZE = 18;
     static final float ADDRESS_MARKER_SIZE = 12.0f;
-    public static final int SIZE = 10;
     static final int LAST_NUMBER_IMAGE_HELP_PAGE = 12;
     static final int WIDTH = 4;
     static final int READY_TO_CALCUL_ROUTE = 2;
+
+
 
     private MapConstants(){}
 }
