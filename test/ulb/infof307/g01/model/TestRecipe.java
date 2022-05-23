bcb9b8ec45aa9911944013db77c8eb3624a29153
Nathan Duriez <NathanDuriez@protonmail.com>
2022-05-23 20:56:06 +0200

    Refactor (opti) TestRecipe
    Co-authored-by: aarfani <aarfani@ulb.be>

--- test/ulb/infof307/g01/model/TestRecipe.java
+++ test/ulb/infof307/g01/model/TestRecipe.java
@@ -1,24 +1,19 @@
 package ulb.infof307.g01.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ulb.infof307.g01.model.Recipe;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 /**
  * test de la classe recipe
  */
 public class TestRecipe {
 
-    private Recipe recipeTest;
-
-    @BeforeEach
-    private void createRecipe() {recipeTest = new Recipe.RecipeBuilder().withId(1).withName("Test").withDuration(3).withCategory("Informatique").withType("Test").withNumberOfPerson(2).withPreparation("Faire le test avant le code").build();
-    }
-
     @Test
     public void testEquals() {
+
+        Recipe recipeTest = new Recipe.RecipeBuilder().withId(1).withName("Test").withDuration(3).withCategory("Informatique").withType("Test").withNumberOfPerson(2).withPreparation("Faire le test avant le code").build();
 
         Recipe recipeTestCopy = new Recipe.RecipeBuilder().withId(1).withName("Test").withDuration(3).withCategory("Informatique").withType("Test").withNumberOfPerson(2).withPreparation("Faire le test avant le code").build();
         assertEquals(recipeTest, recipeTestCopy, "Echec du test de comparaison valide");
