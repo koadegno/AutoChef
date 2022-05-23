0e7d307626f9890de7002ee87df4f51ee73f8110
imoussao27 <imane.moussaoui@ulb.be>
2022-05-23 18:37:59 +0200

    [DEL] print inutile

--- src/main/java/ulb/infof307/g01/controller/Controller.java
+++ src/main/java/ulb/infof307/g01/controller/Controller.java
@@ -94,8 +94,6 @@ public abstract class Controller {
             currentStage.show();
         }
         catch (IOException e) {
-            e.printStackTrace();
-            System.out.println(e);
             URL missingFXMLFile = loader.getLocation();
             ViewController.showErrorFXMLMissing(missingFXMLFile);
         }
